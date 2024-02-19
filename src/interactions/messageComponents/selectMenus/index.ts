cfc90a118970d22284a65cdf9220885d25c52086
Erin Murray <sudo@fur.media>
2024-02-19 18:38:24 +0100

    added custom event handler

--- src/interactions/messageComponents/selectMenus/index.ts
+++ src/interactions/messageComponents/selectMenus/index.ts
@@ -1,8 +1,10 @@
 import { BaseMessageComponent } from "../base";
+import { Custom } from "./custom";
 import { ModContactReportSM } from "./modContactReportSM";
 import { ModContactWizardSelectMenu } from "./modContactWizardSelect";
 
 export const SelectMenus: Array<BaseMessageComponent> = [
+    new Custom,
     new ModContactWizardSelectMenu,
-    new ModContactReportSM
+    new ModContactReportSM,
 ]
